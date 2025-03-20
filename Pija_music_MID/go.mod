c3ca21f57c6627810adf5cb8bde0742f05948138
luisafernada08 <luisagoyeneche09@gmail.com>
2025-03-20 14:45:19 -0500

    creacion del api MID

---
+++ Pija_music_MID/go.mod
@@ -0,0 +1,7 @@
+
+module Pija_music_MID
+
+go 1.23
+
+require github.com/astaxie/beego v1.12.1
+require github.com/smartystreets/goconvey v1.6.4
---
+++ Pija_music_MID/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "Pija_music_MID/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
